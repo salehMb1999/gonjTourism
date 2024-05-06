e8ec0b940cd79d67c683057fa1e5423ec9a45baf
salehMb1999 <salehmirbagheri78@gmail.com>
2024-05-06 02:06:44 -0700

    hero home component responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,20 +7,20 @@ export default function Hero(props) {
       }}
       className={`absolute left-0 right-0 top-0 -z-10 h-screen !bg-cover !bg-top !bg-no-repeat font-iranYekanRegular`}
     >
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-white">
-        <h1 className="mb-10 font-iranYekanBold text-5xl font-bold">
+      <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center text-white">
+        <h1 className="mb-10 font-iranYekanBold text-2xl font-bold md:text-5xl">
           {props.title}
         </h1>
-        <p className="mb-7">{props.text}</p>
-        <div className="flex items-center justify-center gap-5">
+        <p className="mb-7 w-72">{props.text}</p>
+        <div className="flex flex-col items-center justify-center gap-5 md:flex-row">
           <a
-            className="cursor-pointer rounded-md bg-white px-5 py-3 text-black duration-200 ease-in-out hover:bg-[#01959a] hover:text-white"
+            className="w-40 cursor-pointer rounded-md bg-white px-5 py-3 text-black duration-200 ease-in-out hover:bg-[#01959a] hover:text-white"
             href="/docs/gonj-tourism.pdf"
           >
             دانلود کاتالوگ{" "}
           </a>
           <a
-            className="cursor-pointer rounded-md bg-white px-5 py-3 text-black duration-200 ease-in-out hover:bg-[#01959a] hover:text-white"
+            className="w-40 cursor-pointer rounded-md bg-white px-5 py-3 text-black duration-200 ease-in-out hover:bg-[#01959a] hover:text-white"
             href="#"
           >
             قوانین و مقررات{" "}
